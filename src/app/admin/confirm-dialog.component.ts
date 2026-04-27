5c5fecc491b6a3943f55a615bcd610c5cf638868
Clenet18Mi <miloclenet@gmail.com>
2026-04-27 12:22:01 +0200

    split files html,scss,ts

---
+++ src/app/admin/confirm-dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss',
+})
+export class ConfirmDialogComponent {
+  data: { title: string; message: string } = { title: '', message: '' };
+}
